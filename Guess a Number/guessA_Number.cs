9a332e9e243c9c47f7a1ab4fdf73e962fe18d9ac
SimeonMarinov98 <126332378+SimeonMarinov98@users.noreply.github.com>
2023-03-02 19:07:27 +0200

    Add files via upload

---
+++ Guess a Number/guessA_Number.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Guess_a_Number
+{
+    internal class guessA_Number
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber=new Random();
+            int computerNumber=randomNumber.Next(1,101);
+            int playerNumber;
+            while(true)
+            {
+                Console.Write("Guess a number!(1-100)");
+                string input=Console.ReadLine();
+                bool isValid = int.TryParse(input, out playerNumber);
+                if(isValid)
+                {
+                    if(playerNumber==computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber<computerNumber)
+                    {
+                        Console.WriteLine("Too Low.");
+                        
+
+                    }
+                    else if (playerNumber>computerNumber)
+                    {
+                        Console.WriteLine("Too High.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+
+        }
+    }
+}
